4d79ac0c4064542471fc01918f96d5b9dee8c3b9
Tobias <tobias.prepas@gmail.com>
2021-10-04 11:39:37 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
     <div>
       <h1>#Todo</h1>
       <TodoList />
+      <footer>
+        <p>
+          created by{" "}
+          <a href="https://devchallenges.io/portfolio/Tobias235">Tobias235</a> -
+          devChallenges.io
+        </p>
+      </footer>
     </div>
   );
 }
